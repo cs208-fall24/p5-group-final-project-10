7ba78cdab086b50a12358faadd9e3a2a834481aa
DragonWarriorGK <66695024+DragonWarriorGK@users.noreply.github.com>
2024-11-29 16:03:43 -0800

    Added the ability to add comments

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import sql from 'sqlite3'
+import fs from 'fs'
 
 const sqlite3 = sql.verbose()
 
@@ -10,22 +11,50 @@ const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'pug')
-app.use(express.urlencoded({ extended: false }))
+//app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.get('/', function (req, res) {
   console.log('GET called')
   res.render('index')
 })
 
+//-----
 app.get('/student1', function (req, res) {
   console.log('GET called')
   res.render('student1')
+})
+
+app.post('/addComment', function (req, res) {
+  console.log(req.body)
+
+  // The text you want to append
+  const textToAppend = '\n' + req.body.value;
+
+  // The file path (it should be an existing file)
+  const filePath = 'public/Data.txt';
+
+  if (fs.existsSync(filePath)) {
+    console.log('File exists');
+  } else {
+    console.log('File does not exist');
+  }
+
+  // Append the text to the file
+  fs.appendFile(filePath, textToAppend, (err) => {
+  if (err) {
+      console.log('Error appending to file:', err);
+  } else {
+      console.log('Text successfully appended!');
+  }
+  });
 })
 
 app.get('/comments', function (req, res){
   console.log('GET called')
   res.render('student1/comments')
 })
+//-----
 
 app.get('/student2', function (req, res) {
   console.log('GET called')

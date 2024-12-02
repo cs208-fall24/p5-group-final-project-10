f12a9393d73f1bdbd406af67a5d9e111c45ab1b1
Veerdex <grantkoupal@u.boisestate.edu>
2024-12-01 22:09:43 -0700

    Made home page look a bit better

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-// TODO: Add any JavaScript here to make your page fancy :)
+
+const background = document.getElementsByClassName('background');
+
+const height = document.documentElement.scrollHeight - 200;
+
+background.style.top = `${height}px`;

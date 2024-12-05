8fdcb9eee4bc9f3cb4121e601df6a6661550baca
DevynKore <devynkorber@gmail.com>
2024-12-04 20:46:30 -0700

    Working

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'pug')
-//app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // Initialize a table specifically for "Prompt Engineering"
